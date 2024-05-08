2890dbd6e007aa5e1eaf2b699002d9922664b7d6
javid shabin <javid.main.act@gmail.com>
2024-05-08 23:57:24 +0530

    solve unknow list adding proble for the local storage

--- script.js
+++ script.js
@@ -5,37 +5,31 @@ let itemInput = document.getElementById("todo-input");
 // The variable called globaly
 let inputValue = itemInput.value;
 
+// Activity array
+let activityArray = [];
+
 const setLocalStorage = () => {
     // Set local to storage
-    localStorage.setItem("activity", inputValue)
+    localStorage.setItem("activity", inputValue);
 }
 
 const getLocalStorage = () => {
     // Get item from local storage
-    inputValue = localStorage.getItem("activity")
+    // If have any value in the input
+    if (localStorage.getItem("activity")) {
+        inputValue = localStorage.getItem("activity");
+        buildUl();
+    }
 }
 
-// Add you activity
-addBtn.addEventListener("click", () => {
-    inputValue = itemInput.value;
-
-    if (!inputValue) {
-        // If not have any value in the input box
-        alert("Oops!, Enter any activity");
-    }else {
-        // If have any value in the input box
+const buildUl = () => {
+    // If have any value in the input box
         // Create a list and add inputValue
         let list = document.createElement("li");
-        let span = document.createElement("span")
+        let span = document.createElement("span");
         span.innerHTML = inputValue;
         itemInput.value = "";
-        itemInput.focus()
-
-        // Set items to local stoorage
-        setLocalStorage()
-
-        // Get itmes from local storage
-        getLocalStorage()
+        itemInput.focus();
 
         // Create a trash button
         let trash = document.createElement("i");
@@ -47,7 +41,22 @@ addBtn.addEventListener("click", () => {
 
         // Append items
         list.append( span, trash, edit);
-        itemList.appendChild(list)
+        itemList.appendChild(list);
+}
+
+// Add you activity
+addBtn.addEventListener("click", () => {
+    inputValue = itemInput.value;
+
+    if (!inputValue) {
+        // If not have any value in the input box
+        alert("Oops!, Enter any activity");
+    }else {
+        // Set items to local stoorage
+        setLocalStorage();
+
+        // Get itmes from local storage
+        getLocalStorage();
 
     }
 })
@@ -66,8 +75,9 @@ itemList.addEventListener("click", (event) => {
     if (position.classList.contains("fa-edit")) {
         let editValue = prompt("Enet new value");
         let item = position.parentElement;
-        let span = document.querySelector("span")
-        span.innerHTML = editValue
+        let span = document.querySelector("span");
+        span.innerHTML = editValue;
     }
 })
 
+getLocalStorage();
