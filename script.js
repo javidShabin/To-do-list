e13e1b825ba72748f26fac103f2775f5a3dcd600
javid shabin <javid.main.act@gmail.com>
2024-05-08 20:45:51 +0530

    add list use js and add trash and edit buttons

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
 const addBtn = document.getElementById("add-btn");
 let itemList = document.querySelector(".todo-items-list");
 let itemInput = document.getElementById("todo-input");
+
+addBtn.addEventListener("click", () => {
+    let inputValue = itemInput.value;
+
+    if (!inputValue) {
+        // If not have any value in the input box
+        alert("Oops!, Enter any activity");
+    }else {
+        // If have any value in the input box
+        // Create a list and add inputValue
+        let list = document.createElement("li");
+        list.innerHTML = inputValue;
+        
+        // Create a trash button
+        let trash = document.createElement("i");
+        trash.classList.add("fas", "fa-trash");
+        
+        // Create a edit button
+        let edit = document.createElement("i");
+        edit.classList.add("fas", "fa-edit");
+
+        // Append items
+        list.append(trash, edit);
+        itemList.appendChild(list)
+
+    }
+})
